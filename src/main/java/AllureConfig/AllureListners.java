21804d7e51b65b418dccc348eb405cdc8b5ef4b4
Vishwajeet <vishwajeet.kumar@hexaviewtech.com>
2022-11-02 20:51:14 +0530

    Allure Report Jenkins

---
+++ src/main/java/AllureConfig/AllureListners.java
@@ -0,0 +1,86 @@
+/*
+ * package AllureConfig;
+ * 
+ * import org.apache.logging.log4j.LogManager; import
+ * org.apache.logging.log4j.Logger; import org.openqa.selenium.OutputType;
+ * import org.openqa.selenium.TakesScreenshot; import
+ * org.openqa.selenium.WebDriver; import org.testng.ITestContext; import
+ * org.testng.ITestListener; import org.testng.ITestResult;
+ * 
+ * import io.qameta.allure.Attachment;
+ * 
+ * public class AllureTestNGListerners implements ITestListener {
+ * 
+ * private static final Logger logger =
+ * LogManager.getLogger(AllureTestNGListerners.class);
+ * 
+ * private static String getTestMethodName(ITestResult iTestResult) { return
+ * iTestResult.getMethod().getConstructorOrMethod().getName(); }
+ * 
+ * // Text attachments for Allure
+ * 
+ * @Attachment(value = "Page screenshot", type = "image/png") public byte[]
+ * saveScreenshotPNG(WebDriver driver) { return ((TakesScreenshot)
+ * driver).getScreenshotAs(OutputType.BYTES); }
+ * 
+ * // Text attachments for Allure
+ * 
+ * @Attachment(value = "{0}", type = "text/plain") public static String
+ * saveTextLog(String message) { return message; }
+ * 
+ * // HTML attachments for Allure
+ * 
+ * @Attachment(value = "{0}", type = "text/html") public static String
+ * attachHtml(String html) { return html; }
+ * 
+ * @Override public void onStart(ITestContext iTestContext) {
+ * logger.info("I am in onStart method " + iTestContext.getName()); //
+ * iTestContext.setAttribute("WebDriver", this.driver); }
+ * 
+ * @Override public void onFinish(ITestContext iTestContext) {
+ * logger.info("I am in onFinish method " + iTestContext.getName());
+ * 
+ * // Do tier down operations for ExtentReports reporting! //
+ * getExtentReports().flush(); }
+ * 
+ * @Override public void onTestStart(ITestResult iTestResult) {
+ * logger.info(getTestMethodName(iTestResult) + " test is starting."); }
+ * 
+ * @Override public void onTestSuccess(ITestResult iTestResult) {
+ * logger.info(getTestMethodName(iTestResult) + " test is succeed."); //
+ * ExtentReports log operation for passed tests. // getTest().log(Status.PASS,
+ * "Test passed"); }
+ * 
+ * @Override public void onTestFailure(ITestResult iTestResult) {
+ * logger.info(getTestMethodName(iTestResult) + " test is failed.");
+ * 
+ * // Get driver from BaseTest and assign to local webdriver variable. Object
+ * testClass = iTestResult.getInstance(); WebDriver driver = ((BaseClass)
+ * testClass).getLdriver();
+ * 
+ * // Allure ScreenShotRobot and SaveTestLog if (driver != null) {
+ * System.out.println("Screenshot captured for test case:" +
+ * getTestMethodName(iTestResult)); saveScreenshotPNG(driver);
+ * 
+ * // Save a log on allure. saveTextLog(getTestMethodName(iTestResult) +
+ * " failed and screenshot taken!");
+ * 
+ * // Take base64Screenshot screenshot for extent reports
+ * 
+ * String base64Screenshot = "data:image/png;base64," + ((TakesScreenshot)
+ * Objects.requireNonNull(driver)).getScreenshotAs(OutputType.BASE64);
+ * 
+ * // ExtentReports log and screenshot operations for failed tests. //
+ * getTest().log(Status.FAIL, "Test Failed", //
+ * getTest().addScreenCaptureFromBase64String(base64Screenshot).getModel().
+ * getMedia().get(0)); }
+ * 
+ * @Override public void onTestSkipped(ITestResult iTestResult) {
+ * logger.info(getTestMethodName(iTestResult) + " test is skipped."); //
+ * ExtentReports log operation for skipped tests. // getTest().log(Status.SKIP,
+ * "Test Skipped"); }
+ * 
+ * @Override public void onTestFailedButWithinSuccessPercentage(ITestResult
+ * iTestResult) { logger.info("Test failed but it is in defined success ratio "
+ * + getTestMethodName(iTestResult)); } }
+ */
---
+++ src/test/java/allreTestCases/Testingg.java
@@ -0,0 +1,73 @@
+package allreTestCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import AllureConfig.AllureTestNGListerners;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
+
+
+@Listeners(AllureTestNGListerners.class)
+
+public class Testingg {
+
+	@Step("Initialize the ChromeDriver")
+	@BeforeTest(description = "BeforeTest TestNG")
+	public void seetup() throws Exception {
+
+		/*
+		 * setup("chrome");
+		 * getLdriver().navigate().to("https://demo.automationtesting.in/Windows.html");
+		 * getLdriver().manage().window().maximize();
+		 */
+	}
+
+	@Step("TestCase case1 will run")
+	@Description("This is description for case1")
+	@Severity(SeverityLevel.BLOCKER)
+
+	@Test
+	public void case1() {
+
+		Assert.assertEquals(true, false);
+	}
+
+	@Step("TestCase case2 will run")
+	@Description("This is description for case2")
+	@Severity(SeverityLevel.TRIVIAL)
+
+	@Test
+	public void case2() {
+		Assert.assertEquals(true, false);
+
+	}
+
+	@Step("TestCase case3 will run")
+	@Description("This is description for case3")
+	@Severity(SeverityLevel.NORMAL)
+	@Story("skdjhaukshfhasdhf")
+	@Test
+	public void case3() {
+		Assert.assertEquals(true, true);
+
+	}
+
+	@Story("case4")
+	@Step("TestCase case4 will run")
+	@Description("This is description for case4")
+	@Severity(SeverityLevel.NORMAL)
+	@Test
+	public void case4() {
+		System.out.println("***********Testcase 4***************");
+	}
+
+}
